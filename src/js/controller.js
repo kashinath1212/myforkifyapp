507bd53f8fb42963e4b035c3f887d6acb8662eb8
kashinath <115684143+kashinath1212@users.noreply.github.com>
2024-04-13 18:41:11 +0530

    Update controller.js

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,7 +2,6 @@ import * as model from './model.js';
 import recipeViews from '../js/views/recipeViews.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-import { async } from 'regenerator-runtime';
 import searchViews from './views/searchViews.js';
 import resultView from './views/resultView.js';
 import paginationView from './views/paginationView.js';
@@ -10,104 +9,93 @@ import bookmarkView from './views/bookmarkView.js';
 import addRecipeView from './views/addRecipeView.js';
 import { MODAL_CLOSE_SEC } from './config.js';
 
-const recipeContainer = document.querySelector('.recipe');
+document.addEventListener('DOMContentLoaded', function () {
 
-// https://forkify-api.herokuapp.com/v2
+    const recipeContainer = document.querySelector('.recipe');
 
-///////////////////////////////////////
+    const controlRecipe = async function () {
+        try {
+            const id = window.location.hash.slice(1);
+            if (!id) return;
 
-// if (module.hot) {
-//   module.hot.accept();
-// }
+            recipeViews.recipeSpinner();
 
-const controlRecipe = async function () {
-  try {
-    // hashchange
-    const id = window.location.hash.slice(1);
-    if (!id) return;
+            await model.loadRecipe(id);
 
-    // spinner
-    recipeViews.recipeSpinner();
+            recipeViews.render(model.state.recipe);
+        } catch (err) {
+            recipeViews.renderError();
+        }
+    };
 
-    // get recipe
-    await model.loadRecipe(id);
+    const controlSearchResults = async function () {
+        try {
+            const query = searchViews.getQuery();
 
-    //rendering recipe
-    recipeViews.render(model.state.recipe);
-  } catch (err) {
-    recipeViews.renderError();
-  }
-};
+            await model.loadSearchResults(query);
 
-const controlSearchResults = async function () {
-  try {
-    // resultView.recipeSpinner();
-    const query = searchViews.getQuery();
+            resultView.render(model.getSerchResultPerPage());
 
-    await model.loadSearchResults(query);
+            paginationView.render(model.state.search);
+        } catch (err) {
+            throw err;
+        }
+    };
 
-    // resultView.render(model.state.search.results);
-    resultView.render(model.getSerchResultPerPage());
+    const controlPagination = function (goToPage) {
+        resultView.render(model.getSerchResultPerPage(goToPage));
 
-    paginationView.render(model.state.search);
-  } catch (err) {
-    throw err;
-  }
-};
+        paginationView.render(model.state.search);
+    };
 
-const controlPagination = function (goToPage) {
-  resultView.render(model.getSerchResultPerPage(goToPage));
+    const controlServings = function (newServings) {
+        model.updateServings(newServings);
+        recipeViews.render(model.state.recipe);
+    };
 
-  paginationView.render(model.state.search);
-};
+    const controlAddBookmark = function () {
+        if (!model.state.recipe.bookmarked) {
+            model.addBookmark(model.state.recipe);
+        } else {
+            model.removeBookmark(model.state.recipe.id);
+        }
+        recipeViews.render(model.state.recipe);
+        bookmarkView.render(model.state.bookmarks);
+    };
 
-const controlServings = function (newServings) {
-  //Update the recipe servings (in state)
-  model.updateServings(newServings);
-  //render updated recipe
-  recipeViews.render(model.state.recipe);
-};
+    const controlBookmark = function () {
+        bookmarkView.render(model.state.bookmarks);
+    };
 
-const controlAddBookmark = function () {
-  if (!model.state.recipe.bookmarked) {
-    model.addBookmark(model.state.recipe);
-  } else {
-    model.removeBookmark(model.state.recipe.id);
-  }
-  recipeViews.render(model.state.recipe);
-  bookmarkView.render(model.state.bookmarks);
-};
+    const controlAddRecipe = async function (newRecipe) {
+        try {
+            recipeViews.recipeSpinner();
+            await model.uploadRecipe(newRecipe);
 
-const controlBookmark = function () {
-  bookmarkView.render(model.state.bookmarks);
-};
+            recipeViews.render(model.state.recipe);
+            addRecipeView.renderMessage();
+            bookmarkView.render(model.state.bookmarks);
 
-const controlAddRecipe = async function (newRecipe) {
-  try {
-    recipeViews.recipeSpinner();
-    await model.uploadRecipe(newRecipe);
-    console.log(model.state.recipe);
-    recipeViews.render(model.state.recipe);
-    addRecipeView.renderMessage();
-    bookmarkView.render(model.state.bookmarks);
-    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+            window.history.pushState(null, '', `#${model.state.recipe.id}`);
 
-    setTimeout(function () {
-      addRecipeView.toggleWindow();
-    }, MODAL_CLOSE_SEC * 1000);
-  } catch (err) {
-    addRecipeView.renderError(err.message);
-  }
-};
+            setTimeout(function () {
+                addRecipeView.toggleWindow();
+            }, MODAL_CLOSE_SEC * 1000);
+        } catch (err) {
+            addRecipeView.renderError(err.message);
+        }
+    };
 
-const Init = function () {
-  bookmarkView.addHandleRender(controlBookmark);
-  recipeViews.addHandlerRender(controlRecipe);
-  recipeViews.addHandlerUpdatingServings(controlServings);
-  recipeViews.addHandlerBookmark(controlAddBookmark);
-  searchViews.addhandlerSearch(controlSearchResults);
-  paginationView._addHandlerClick(controlPagination);
-  addRecipeView.addHandlerUpload(controlAddRecipe);
-};
+    const Init = function () {
+        bookmarkView.addHandleRender(controlBookmark);
+        recipeViews.addHandlerRender(controlRecipe);
+        recipeViews.addHandlerUpdatingServings(controlServings);
+        recipeViews.addHandlerBookmark(controlAddBookmark);
+        searchViews.addhandlerSearch(controlSearchResults);
+        paginationView._addHandlerClick(controlPagination);
+        addRecipeView.addHandlerUpload(controlAddRecipe);
+    };
 
-Init();
+    Init();
+
+});
